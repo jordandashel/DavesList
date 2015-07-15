f89434ef1225ea0fd670871ac1a7e230344d4b9e
holdtb <Holdt.Bryan@gmail.com>
2015-07-15 16:12:15 -0700

    Day 2 complete.

--- app.rb
+++ app.rb
@@ -8,8 +8,6 @@ class Daveslist < Sinatra::Base
   configure do
     MongoMapper.connection =  Mongo::Connection.new("localhost", 27017)
     MongoMapper.database = "DavesRepo"
-
-
   end
 
 
@@ -18,6 +16,27 @@ class Daveslist < Sinatra::Base
     erb :index
   end
 
+  get '/details/:id' do
+    @listing = Listing.find(params[:id])
+    erb :details
+  end
+
+  get '/create' do
+    erb :create
+  end
+
+  post '/create' do
+    newListing = Listing.create!(:title => params[:title],
+    :author => params[:author],
+    :description => params[:description],
+    :price => params[:price],
+    :date_listed => params[:date])
+     redirect "/details/#{newListing._id}"
+  end
+
+  get '/delete/:id' do
+
+  end
 
 
 end
--- model/listing.rb
+++ model/listing.rb
@@ -1,13 +1,15 @@
 require 'mongo_mapper'
 
-class Listing 
+class Listing
   include MongoMapper::Document
 
+  set_collection_name "Listing"
+  
   #Attributes
   key :title, String, :required => true
   key :author, String
-  key :description, String 
+  key :description, String
   key :price, Float
   key :date_listed, Date
-  
+
 end
